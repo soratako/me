186717e2086d8e0e262961dce90ab2c2d4fe87d0
soratako <soratako+soratako@users.noreply.github.com>
2020-09-09 18:29:53 +0900

    hibouchushou

---
+++ heart/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html><html prefix="og: http://ogp.me/ns# website: http://ogp.me/ns/website#" dir="ltr" lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="author" content="Soratako">
+   <meta name="format-detection" content="telephone=no, email=no, address=no">
+  <!-- Bot -->
+  <meta name="robots" content="all" />
+  <!-- End Bot -->
+  <!-- meta -->
+  <meta name="keywords" content="誹謗中傷">
+  <meta name="description" content="誹謗中傷に関するそらたこの考え">
+  <!-- End meta -->
+  <!-- ogp, Twitter tags -->
+  <meta property="og:title" content="誹謗中傷に関するそらたこの考え">
+  <meta property="og:description" content="誹謗中傷に関するそらたこの考え">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://soratako.yunachannel.com/heart/">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:creator" content="@Sierra9Indiaa">
+  <!-- /ogp, Twitter tags -->
+
+  <title>誹謗中傷に関するそらたこの考え</title>
+  <link rel="canonical" href="https://soratako.yunachannel.com/heart/">
+  <!-- Exte JS CSS -->
+  <link rel="stylesheet" href="/style.css" />
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
+  <link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet">
+  <!-- End Exte JS CSS -->
+</head>
+<body>
+<div id="loading" class="loading"><i class="fa fa-spinner fa-spin"></i></div>
+  <script>
+       const loading =  document.getElementById("loading");
+　　window.addEventListener('load', function () {
+　　loading.style.display = "none";
+　　});
+    </script>
+  <div id="contents">
+  <h1>誹謗中傷に関するそらたこの考え</h1>
+<!-- body -->
+<!-- End body -->
+<div class="footer">
+<p id="copyright">
+&copy;(C) 2020<script>new Date().getFullYear()>2010&&document.write("-"+new Date().getFullYear());</script> Soratako.
+</p>
+</div>
+</div>
+</body>
+</html>
