3aaf050310196dce0f830a85b4160bb0b7f492ee
soratako <soratako+soratako@users.noreply.github.com>
2020-09-10 23:49:43 +0900

    hibouchushou

--- heart/index.php
+++ heart/index.php
@@ -35,16 +35,84 @@
 　　window.addEventListener('load', function () {
 　　loading.style.display = "none";
 　　});
-    </script>
-  <div id="contents">
-  <h1>誹謗中傷に関するそらたこの考え</h1>
+  </script>
+<div class="heart" id="contents">
+<h1>誹謗中傷に関するそらたこの考え</h1>
 <!-- body -->
+<?php 
+  $modifiedtimen = new DateTime("2020-09-10");
+  $modifiedtime = $modifiedtimen->format(DateTime::ATOM);
+?>
+ <div class="date-tags">
+  <span class="fas fa-history" aria-hidden="true"></span> <time class="updated" datetime="<?php echo stripslashes($modifiedtime); ?>" itemprop="dateModified"><?php 
+  $dateformat = DateTimeInterface::ATOM;
+  $modifiedtimeformat = DateTime::createFromFormat($dateformat,$modifiedtime);
+  echo $modifiedtimeformat->format("Y.m.d"); ?></time> <span class="far fa-clock" aria-hidden="true"></span> <span class="published"><meta itemprop="datePublished" content="2020-09-10T21:50:27+09:00">2020.09.10</span>
+  </div>
+<p>2020年現在、インターネット上でよく見られる、「誹謗中傷」についてのそらたこの考えです。</p>
+<p>Tik TokなどのSNSでよく見かける言葉として、「有名になったら誹謗中傷は仕方ない」、「誹謗中傷される覚悟を持って投稿をするべきだ」というものがあります。私はこの言葉を聞いて、その通りだと思います。有名になるということはより多くの人の目に入ると言うことです。世の中にはさまざまな考えがあり、1人1人の考えは皆違います。自分にとっての当たり前のことは自分以外の全ての人にとっての当たり前ではありません。他の誰かにとってはあり得ないことかもしれません。さまざまな考えを持つ人の中には誹謗中傷と気づかず、相手を傷つけていると知らずに発言している人もいます。相手を罵って誹謗中傷することを生きがいとしているような頭のおかしい人もいます。YouTubeの低評価で考えてみてください。10万、100万再生されている動画で低評価が1つもついていないものがありますか？まずないと言っていいでしょう。それが見たあなたにとってどんなにいい動画であったとしても他の誰かにとっては低評価をつけるに値する動画であったということです。このように自分と違う考えを持つ人が必ずいることを認めなくてはいけません。有名になるということはより多くの人の目にとまることです。インターネットに投稿をするということはいくら自分がそれを意図していないとしても自分以外の誰かに向けて発信するということです。自分と違う考えを持つ人が自分が投稿、発信した内容を目にしたとき、誹謗中傷をしてくる可能性は十分あります。より多くの人がその投稿、発信を見るということはより誰かに誹謗中傷される可能性が高まるということです。そのため、インターネットに投稿すること、有名になること、には誹謗中傷される覚悟を持つ必要があると思います。<br>
+では、それらの誹謗中傷を当たり前にしていいのでしょうか？当たり前にしてはいけないと私は思います。自分と違う、自分の当たり前と違う、それを認めず、批判する発言が誹謗中傷につながります。相手の考えを認めそれを尊重することができなければ、自分が誹謗中傷をする側になってしまいます。それぞれが相手の考えを認め合う必要があります。Tik TokやYouTubeなどではコメント欄を閉鎖することで誹謗中傷をされないことができます。しかし、これは誹謗中傷以外の意見や感想も一律に省いてしまいます。Tik TokやTwitterなどでは特定の自分が繋がっている人からのコメントのみを受けつけつけることができます。それらの機能を使うことは逃げではありません。誹謗中傷は有名になった証であるという言い方をする人がいます。確かにそういう言い方もできると思います。より多くの人の目について、より多くの視点から意見がよせられる、その意見の中に誹謗中傷も含まれているかもしれません。しかし、それは当たり前にしていいことではありません。<p>
+
+<p>インターネットで他人を悪くいう内容や他人をばかにする内容を書込むことは名誉毀損に当たります。名誉毀損をすると賠償を命じられる可能性があります。安易な発言が誹謗中傷とみなされて、賠償をしなければいけなくなるのです。インターネットでの誹謗中傷は、絶対にやめましょう。インターネットは便利なツールですが、非常に厄介で奥の深いツールでもあります。インターネットでは一度送信したことは取り消すことができません。永遠に残り続けます。投稿の削除やアカウントの削除をしても自分がした発言は完全には消えません。安易な発言は自分の身を滅ぼします。</p>
 <!-- End body -->
+<footer>
+<div id="back-to-previous" class="back-to-previous" style="display:inline-block;">
+      <a href="javascript:history.go(-1);" class="back-to-previous-button"><span class="fas fa-arrow-left"></span></a>
+  </div>
+<div id="back-to-next" class="back-to-next" style="display:inline-block;">
+      <a href="javascript:history.go(+1);" class="back-to-next-button"><span class="fas fa-arrow-right"></span></a>
+  </div>
+<div id="home-menu" class="home-menu" style="display:inline-block;">
+      <a href="https://soratako.yunachannel.com/" class="home-menu-button"><span class="fas fa-home"></span></a>
+  </div>
+<div id="go-to-top" class="go-to-top" style="display:inline-block;">
+      <a class="go-to-top-button"><span class="fas fa-arrow-circle-up"></span></a>
+  </div>
+</footer>
+<script>
+const gototop = document.getElementById("go-to-top");
+gototop.addEventListener("click", goToTop, false);
+function goToTop() {
+var x1 = x2 = x3 = 0;
+var y1 = y2 = y3 = 0;
+if (document.documentElement) {
+x1 = document.documentElement.scrollLeft || 0;
+y1 = document.documentElement.scrollTop || 0;
+}
+if (document.body) {
+x2 = document.body.scrollLeft || 0;
+y2 = document.body.scrollTop || 0;
+}
+x3 = window.scrollX || 0;
+y3 = window.scrollY || 0;
+var x = Math.max(x1, Math.max(x2, x3));
+var y = Math.max(y1, Math.max(y2, y3));
+window.scrollTo(Math.floor(x / 2), Math.floor(y / 2));
+if (x > 0 || y > 0) {
+window.setTimeout("goToTop()", 80);
+}
+}
+window.addEventListener("load", historyDisable, false);
+function historyDisable(){
+    browsehistory = window.history;
+    let browsehistorycount = browsehistory.length - 1;
+    if ( browsehistorycount ) {
+	
+    }else{
+	const historybuttonprev = document.getElementById("back-to-previous");
+	const historybuttonnext = document.getElementById("back-to-next");
+	historybuttonprev.innerHTML = '<div id="fasdisabled" class="back-to-previous-button"><span class="fas fa-arrow-left"></span></div>';
+	historybuttonnext.innerHTML = '<div id="fasdisabled" class="back-to-next-button"><span class="fas fa-arrow-right"></span></div>';
+    }
+}
+</script>
 <div class="footer">
 <p id="copyright">
-&copy;(C) 2020<script>new Date().getFullYear()>2010&&document.write("-"+new Date().getFullYear());</script> Soratako.
+&copy;(C) 2020<script>new Date().getFullYear()>2020&&document.write("-"+new Date().getFullYear());</script> Soratako.
 </p>
 </div>
+<!-- div contents -->
 </div>
+<!-- /div contents -->
 </body>
 </html>
