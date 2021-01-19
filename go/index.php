b7863c6ba7e1b2375dcbee708cdda853a1919f30
soratako <55989784+soratako@users.noreply.github.com>
2021-01-19 16:18:52 +0900

    Update go

--- go/index.php
+++ go/index.php
@@ -1,43 +1,48 @@
 <?php
+ /**
+  * Redirects to the specified URL.
+  *
+  * @author  Soratako <soratako@soratako.yunachannel.com>
+  */
 date_default_timezone_set("Asia/Tokyo");
-$user_IP = getenv("REMOTE_ADDR");
-$accessdate = date("Y-m-d H:i:s");
-$user_agent = getenv("HTTP_USER_AGENT");
-if (!empty($_GET["u"])){
-$to_link = $_GET["u"];
-$get_contents = @file_get_contents($to_link);
-if($get_contents):
-if (!empty($_GET["confirm"])):?>
-<!DOCTYPE html><html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>リンク先の確認</title>
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <meta name="author" content="Soratako">
-  <meta name="format-detection" content="telephone=no, email=no, address=no">
-  <meta name="robots" content="none" />
-</head><body><h1>リンク先の確認</h1><p>以下のURLに遷移しようとしています。<br>よろしければリンクをクリックしてください。</p>
-<a href="<?= $to_link ?>" rel="noreferrer nofollow noopener external"><?= $to_link ?></a>
-</body></html>
-<?php else:
-header("Location: {$to_link}");
-exit;
-endif;
-else:?>
-<!DOCTYPE html><html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>リンク先の確認</title>
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <meta name="author" content="Soratako">
-  <meta name="format-detection" content="telephone=no, email=no, address=no">
-  <meta name="robots" content="none" />
-</head><body><h1>ページが見つかりません</h1><p>お探しのページは存在しないようです。</p>
-<a herf="<?= $to_link ?>" rel="noreferrer nofollow noopener external"><?= $to_link ?></a>
-</body></html>
-<?php
-endif;
-}else{
-echo "エラー";
+$eIPAddress = getenv("REMOTE_ADDR");
+$eAccessDate = date("Y-m-d H:i:s");
+$eUserAgent = getenv("HTTP_USER_AGENT");
+require_once "vendor/autoload.php";
+$twig_loader = new \Twig\Loader\FilesystemLoader("templates");
+$go_twig = new \Twig\Environment($twig_loader);
+if ( !empty ($_GET["u"]) ) {
+    $goGetLink = $_GET["u"];
+    $goToContents = @file_get_contents($goGetLink);
+    if ( $goToContents ) {
+        if ( !empty ($_GET["confirm"]) ) {
+            $goToLink = htmlspecialchars($goGetLink, ENT_QUOTES);
+            $goBackLink = htmlspecialchars(getenv("HTTP_REFERER"), ENT_QUOTES);
+            $twig_data = array(
+                "goTitle" => "リンク先の確認",
+                "goMessage" => "以下のURLに遷移しようとしています。よろしければリンクをクリックしてください。",
+                "goToLink" => $goToLink,
+                "goBackLink" => $goBackLink,
+            );
+            echo $go_twig->render("go.html", $twig_data);
+            exit;
+        } else {
+            header("Location: {$goGetLink}", true, 307);
+            exit;
+        }
+    } else {
+        $goToLink = htmlspecialchars($goGetLink, ENT_QUOTES);
+        $twig_data = array(
+            "goTitle" => "ページが見つかりません",
+            "goMessage" => "お探しのページは存在しないようです。",
+            "goToLink" => $goToLink,
+            "goBackLink" => $goBackLink,
+        );
+        echo $go_twig->render("go.html", $twig_data);
+        exit;
+    }
+} else {
+    echo "エラー";
+    exit(1);
 }
 ?>
